b030a3623846e3fc64fa6a1c9060041ae4b5e056
Allen <chuanshangge@gmail.com>
2018-10-29 12:19:41 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
 import discord
+import urllib.request, json
+import urllib3
+urllib3.disable_warnings()
+from collections import defaultdict
+import operator
+import requests
+from ast import literal_eval
+
 #print(discord.__version__)  # check to make sure at least once you're on the right version!
 
 client = discord.Client()
@@ -17,23 +25,17 @@ async def on_message(message):  # event that happens per any message.
 client.run("NTA0Nzg4NjA3NDQyMjIzMTM0.DrKZBw.gYtkzt_4qFZDOf2Id1OQ_5th3dk")
 
 
-#champion.gg
-import urllib3
-urllib3.disable_warnings()
-'''
-http = urllib3.PoolManager()
-heroes = http.request('GET', 'http://api.champion.gg/champion/annie/matchup?api_key=d143ded682d41d1e3ebe173d0b327e46')
-heroes_dict = json.loads(heroes.data.decode('UTF-8'))
-print(heroes_dict)
-for hero in heroes_dict:
-    print(hero['localized_name'])
-'''
-
-import requests
-
 url = 'http://api.champion.gg/champion/annie/matchup?api_key=d143ded682d41d1e3ebe173d0b327e46'
 response = requests.get(url, verify=True)
 data = response.json();
-for c in data['role']['matchups']:
-    print(c['games'], c['key'])
+result = []
+for item in data:
+    if (item.get('role') == 'Middle'):
+       for x in item.get("matchups"):
+           result.append(x)
+
+#sorted_d = sorted(result.items(), key=operator.itemgetter(0))
+
+for x in sorted(result, key = lambda i: i['winRate'], reverse=True):
+    print(x.get('key'), x.get('winRate'), x.get('games'))
 
