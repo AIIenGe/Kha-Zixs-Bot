51d3fbb5383fde3cde6ccf9eaf0246c8d9521191
Allen <chuanshangge@gmail.com>
2018-10-28 23:02:14 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,7 +11,29 @@ async def on_ready():
 async def on_message(message):  # event that happens per any message.
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
     if "hello" in message.content:
-        await client.send_message(message.channel, 'hello', tts=True)
+        await client.send_message(message.channel, 'Goodbye', tts=True)
 
 
 client.run("NTA0Nzg4NjA3NDQyMjIzMTM0.DrKZBw.gYtkzt_4qFZDOf2Id1OQ_5th3dk")
+
+
+#champion.gg
+import urllib3
+urllib3.disable_warnings()
+'''
+http = urllib3.PoolManager()
+heroes = http.request('GET', 'http://api.champion.gg/champion/annie/matchup?api_key=d143ded682d41d1e3ebe173d0b327e46')
+heroes_dict = json.loads(heroes.data.decode('UTF-8'))
+print(heroes_dict)
+for hero in heroes_dict:
+    print(hero['localized_name'])
+'''
+
+import requests
+
+url = 'http://api.champion.gg/champion/annie/matchup?api_key=d143ded682d41d1e3ebe173d0b327e46'
+response = requests.get(url, verify=True)
+data = response.json();
+for c in data['role']['matchups']:
+    print(c['games'], c['key'])
+
